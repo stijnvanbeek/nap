8a442296009eba189a859e38ce0643f0e40453cc
Stijn van Beek <stijn.v.beek@gmail.com>
2025-09-09 16:04:18 +0200

    check build env use system python3

--- check_build_environment.sh
+++ check_build_environment.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 nap_root=$( cd "$(dirname -- "$0")" ; pwd -P )
-. $nap_root/tools/buildsystem/common/sh_shared.sh
-configure_python $nap_root
-$python $nap_root/tools/buildsystem/check_build_environment/source_user_onboarding.py
-cmake --help
-echo ---- printenv -----
-printenv
-echo ---- set ----
-set
+#. $nap_root/tools/buildsystem/common/sh_shared.sh
+#configure_python $nap_root
+python3 $nap_root/tools/buildsystem/check_build_environment/source_user_onboarding.py
 
 
